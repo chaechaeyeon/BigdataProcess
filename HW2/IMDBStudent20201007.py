a89a0bc78650cbd0cf81fa8b83c460e945a81b7a
chaechaeyeon <00dreamadream@kakao.com>
2023-11-08 21:55:07 +0900

    imdb 수정

--- HW2/IMDBStudent20201007.py
+++ HW2/IMDBStudent20201007.py
@@ -9,8 +9,8 @@ genreCnt = {} #딕셔너리
 with open(inputFile,"rt") as f:
     
     for line in f:
-        movie_array = line.split('::') #::기준으로 자르기 
-        gen_arr = movie_array[2].split("|") #장르 부분 | 나누기
+        movie_array = line.strip().split('::') #::기준으로 자르기 
+        gen_arr = movie_array[2].strip().split("|") #장르 부분 | 나누기
 
         for genre in gen_arr:
             if genre in genreCnt:
@@ -21,4 +21,4 @@ with open(inputFile,"rt") as f:
 
 with open(outputFile,"wt") as output:
     for genre, count in genreCnt.items():
-        output.write("%s %d\n" %(genre, count))
+        output.write("%s %d\n" %(genre,count))
